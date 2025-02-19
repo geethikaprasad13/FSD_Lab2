98bdae447958a92ec88f524c5a055b7f23f2689f
geethikaprasad13 <geethikaprasad13@gmail.com>
2025-02-19 11:47:46 +0530

    Final Commit

--- http_HTML.js
+++ http_HTML.js
@@ -1,26 +1,10 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const port = 3000;
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/index.html') {
-    res.setHeader('Content-Type', 'text/html');
-    const filePath = path.join(__dirname, 'index.html');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end('Error reading file');
-      } else {
-        res.statusCode = 200;
-        res.end(data);
-      }
-    });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');
-  }
+const express = require('express');
+const app = express();
+app.get('/index',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
 });
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
